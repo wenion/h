ec64e995d717f73f1396c281578746d63bc73d6f
SteveLEEEEE <yuheng.li@monash.edu>
2024-06-08 16:29:35 +1000

    Request for creating or deleting Process Model

--- h/models_redis/process_model.py
+++ h/models_redis/process_model.py
@@ -7,11 +7,12 @@ class ProcessModel(JsonModel):
     class Meta:
         global_key_prefix = 'h'
         model_key_prefix = 'ProcessModel'
-    creator: str = Field(index=True) #userid in UserRole
-    create_time: int = Field(index=True) # the time process model is created
-    group: str = Field(index=True) #the permitted groups for the ShareFlow public_id
-    pm_name: str = Field(index=True)#process model name
-    pm_content: str = Field(index=True)# process model content
+    creator: str = Field(index=True)  # userid in UserRole
+    create_time: int = Field(index=True)  # the time process model is created
+    group: str = Field(index=True)  # the permitted groups for the ShareFlow public_id
+    pm_name: str = Field(index=True)  # process model name
+    pm_content: str = Field(index=True)  # process model content
+    session_id: str = Field(index=True) # session_id for the ShareFlow
 
 
 def fetch_all_process_model():
@@ -20,8 +21,8 @@ def fetch_all_process_model():
     return all_models if len(all_models) > 0 else None
 
 
-def fetch_process_model_by_name_creator(name, creator):
-    query = ProcessModel.find((ProcessModel.pm_name == name) & (ProcessModel.creator == creator))
+def fetch_process_model_by_session_creator(session_id, creator):
+    query = ProcessModel.find((ProcessModel.session_id == session_id) & (ProcessModel.creator == creator))
     total = query.all()
     return total[0] if len(total) > 0 else None
 
@@ -37,20 +38,26 @@ def create_process_model(
         create_time,
         group,
         pm_name,
-        pm_content,):
+        pm_content,
+        session_id):
+    exist = fetch_process_model_by_session_creator(session_id, creator)
+    if exist:
+        print("PM already exist.")
+        return exist
     process_model = ProcessModel(
         creator = creator,
         create_time = create_time,
         group = group,
         pm_name = pm_name,
         pm_content = pm_content,
+        session_id = session_id
     )
     process_model.save()
     return process_model
 
 
-def update_process_model(name, creator, update):
-    process_model = fetch_process_model_by_name_creator(name, creator)
+def update_process_model(session_id, creator, update):
+    process_model = fetch_process_model_by_session_creator(session_id=session_id, creator=creator)
     if process_model:
         # process_model.creator = update.get('creator')
         # process_model.group = update.get('group')
@@ -63,10 +70,13 @@ def update_process_model(name, creator, update):
         return None
 
 
-def delete_process_model(name, creator):
+def delete_process_model_by_session_creator(session_id, creator):
     try:
-        pm = fetch_process_model_by_name_creator(name, creator)
-        pm.delete()
+        pm = fetch_process_model_by_session_creator(session_id, creator)
+        if pm:
+            ProcessModel.delete(pm.pk)
+        else:
+            print("Cannot find PM to be deleted.")
     except:
         return False
     else:
@@ -78,5 +88,3 @@ def delete_process_model(pk):
         ProcessModel.delete(pk)
     except:
         return False
-    else:
-        return True
