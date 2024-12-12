a959d2b542ad9683fce1ef4f403ec364cbd76269
jim wen <jim.wen@monash.edu>
2024-12-13 06:31:31 +1100

    Delete test files

--- tests/common/factories/activation.py
+++
@@ -1,9 +0,0 @@
-from h import models
-
-from .base import ModelFactory
-
-
-class Activation(ModelFactory):
-    class Meta:
-        model = models.Activation
-        sqlalchemy_session_persistence = "flush"
--- tests/unit/h/models/helpers_test.py
+++
@@ -1,9 +0,0 @@
-from unittest.mock import Mock
-
-from h.models import helpers
-
-
-def test_repr_():
-    obj = Mock(foo="FOO", bar="BAR")
-
-    assert helpers.repr_(obj, ["foo", "bar"]) == "Mock(foo='FOO', bar='BAR')"
